4341fb3f4dcbe55551e7000470662a58146ff7de
“牧涯” <muya.wss@alibaba-inc.com>
2020-04-09 15:23:31 +0800

    init

---
+++ src/layouts/BasicLayout/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-wrapper">
+    <div>双双本地的小二后台子项目（基于 Vue）</div>
+    <div class="main-container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style>
+.main-container {
+  background-color: rgb(255, 255, 255);
+  border-radius: 6px;
+  padding: 20px;
+  margin: 20px 0;
+}
+</style>
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>小二：首页  state.count: {{ count }} </div>
+    <router-link to="/detail">detail</router-link>
+    <br />
+    <router-link to="/list">list</router-link>
+    <br />
+    <el-button type="primary" @click="handleClick">子应用间跳转</el-button>
+    <el-alert
+      title="这是一个 Element 的组件"
+      type="success"
+    >
+    </el-alert>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters(['count'])
+  },
+  methods: {
+    handleClick: () => {
+      window.history.pushState({}, '', '/dashboard');
+    }
+  }
+}
+</script>>
